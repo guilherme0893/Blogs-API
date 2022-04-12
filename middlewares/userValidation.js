ddc87de53022e06276f3bd4029c5287e999dd04d
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 17:08:51 -0300

    feat: Creates displayName & email validations in userValidation middleware

---
+++ middlewares/userValidation.js
@@ -0,0 +1,20 @@
+const displayNameValidation = (req, res, next) => {
+  const { displayName } = req.body;
+  if (displayName.length < 8) {
+    res.status(400).json({ message: '"displayName" length must be at least 8 characters long' });
+  }
+  next();
+};
+
+const emailValidation = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: '"email" is required' });
+  }
+  // regex reference https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+  const emailRegex = /\S+@\S+\.\S+/;
+  if (!emailRegex.test(email) || email === '@gmail.com') {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+  next();
+};
