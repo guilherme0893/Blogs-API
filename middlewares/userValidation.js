9467ce9ac616df4924cc8da9eca4e59897c4e1cd
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 18:57:34 -0300

    fix: Add return to displayNameValidation function following test fail

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -1,7 +1,8 @@
 const displayNameValidation = (req, res, next) => {
   const { displayName } = req.body;
   if (displayName.length < 8) {
-    res.status(400).json({ message: '"displayName" length must be at least 8 characters long' });
+    return res.status(400)
+      .json({ message: '"displayName" length must be at least 8 characters long' });
   }
   next();
 };
