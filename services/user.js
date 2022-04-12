4bfd35d9b95e9930032865d330894d4b9b251e8b
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 16:24:44 -0300

    feat: Create user service checking email existence

---
+++ services/user.js
@@ -0,0 +1,17 @@
+const { User } = require('../models');
+
+const createUser = async ({ displayName, email, password, image }) => {
+  try {
+    // check email --> regra de negocio 
+    const checkEmail = await User.findOne({ where: { email } });
+    if (checkEmail) return 'User already registered';
+    if (!checkEmail) {
+      const user = await User.create({ displayName, email, password, image });
+      return user;  
+    }  
+  } catch (error) {
+    console.error(error);    
+  }
+};
+
+module.exports = createUser;
