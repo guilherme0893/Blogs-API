26975cdcc5e1f4eb6e7519755167602ea06df13a
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 17:21:53 -0300

    feat: Create user verification in the user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,12 @@
 const User = require('../services/user');
 
-const createUser = async (req, _res) => {
+const createUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
   try {
     const user = User.createUser({ displayName, email, password, image });
+    if (user) {
+      return res.status(409).json({ message: 'User already registered' });
+    }
     return user;    
   } catch (error) {
     console.error(error);
