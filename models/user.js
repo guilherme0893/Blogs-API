4856d4d9c495ba4dbaab4e33916a188cd1fcc460
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 15:27:23 -0300

    chore: Defines user models & migrations

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+  return User;
+};
+
+module.export = User;
