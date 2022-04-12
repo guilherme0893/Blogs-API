a2323e79e2d9fb72818d0e47669340ae89fd993e
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-12 18:10:55 -0300

    feat: Create passwordValidation in userValidation middleware

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -18,3 +18,14 @@ const emailValidation = (req, res, next) => {
   }
   next();
 };
+
+const passwordValidation = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
+  if (password.length === 6) {
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+  }
+  next();
+};
