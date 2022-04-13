c6a1a900cee8757c31197157b07428584a9fa3ae
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-13 13:16:30 -0300

    fix: Fix createToken logic

--- services/tokenGenerator.js
+++ services/tokenGenerator.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const jwtConfig = {
-  expiresIn: '7d',
-  algorithm: 'HS256',
-};
-
-const secret = 'seusecretdetoken';
-
 function createToken(user) {
+  const jwtConfig = {
+    expiresIn: '15m',
+    algorithm: 'HS256',
+  };
+  const secret = 'seusecretdetoken';
   const token = jwt.sign({ data: user }, secret, jwtConfig);
   return token;
 }
 
-module.exports = createToken;
+module.exports = { 
+  createToken,
+};
