21158c25a65dcab4cc52e3c6351386e8bfcb093c
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-13 14:47:23 -0300

    fix: Fix userValidation logic and order

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -9,11 +9,11 @@ const displayNameValidation = (req, res, next) => {
 
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
-  if (!email) {
-    return res.status(400).json({ message: '"email" is required' });
-  }
   if (email === '') {
     return res.status(400).json({ message: '"email" is not allowed to be empty' });
+  }
+  if (!email) {
+    return res.status(400).json({ message: '"email" is required' });
   }
   // regex reference https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
   const emailRegex = /\S+@\S+\.\S+/;
@@ -25,11 +25,11 @@ const emailValidation = (req, res, next) => {
 
 const passwordValidation = (req, res, next) => {
   const { password } = req.body;
-  if (!password) {
-    return res.status(400).json({ message: '"password" is required' });
-  }
   if (password === '') {
     return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
   }
   if (password.length !== 6) {
     return res.status(400).json({ message: '"password" length must be 6 characters long' });
