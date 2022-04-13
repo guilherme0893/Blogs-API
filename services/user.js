ea19d0184594fcdb9e7aed156a93fc3a4ed3e54b
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-13 16:21:28 -0300

    chore: Create getAllUsers in services & removes skip from test

--- services/user.js
+++ services/user.js
@@ -2,15 +2,9 @@ const { User } = require('../models');
 
 const createUser = async ({ displayName, email, password, image }) => {
   try {
-    // check email --> regra de negocio 
     const checkEmail = await User.findOne({ where: { email } });
-    // console.log(User.findOne({ where: { email } }));
-    // console.log(email);
-    // console.log(checkEmail);
-    // if (checkEmail) return 'User already registered';
     if (!checkEmail) {
       const user = await User.create({ displayName, email, password, image });
-      // console.log(user, 'olha o user aqui');
       return user;  
     }  
   } catch (error) {
@@ -18,6 +12,16 @@ const createUser = async ({ displayName, email, password, image }) => {
   }
 };
 
+const getAllUsers = async () => {
+  try {
+    const users = await User.findAll();
+    return users;    
+  } catch (error) {
+    console.error(error);    
+  }
+};
+
 module.exports = {
   createUser,
+  getAllUsers,
 };
