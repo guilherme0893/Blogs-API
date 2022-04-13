4efeec7728b5a954bd5fd44a51511c4c2f8dab14
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-13 16:47:48 -0300

    fix: Add export of secret in tokenGenerator.js

--- services/tokenGenerator.js
+++ services/tokenGenerator.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
 
+const jwtConfig = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const secret = 'seusecretdetoken';
+
 function createToken(user) {
-  const jwtConfig = {
-    expiresIn: '15m',
-    algorithm: 'HS256',
-  };
-  const secret = 'seusecretdetoken';
   const token = jwt.sign({ data: user }, secret, jwtConfig);
   return token;
 }
 
 module.exports = { 
   createToken,
+  secret,
 };
