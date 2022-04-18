d0914452600dde26b6c8911d71d7699a40f79622
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-18 16:13:42 -0300

    feat: Drop DB and recreate tables of categories and users -- starts task 05

---
+++ models/category.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+  return Category;
+};
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Category extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   Category.init({
+//     name: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'Category',
+//   });
+//   return Category;
+// };
