db57cf078b451f1e8936ed6add693fd2cf379a57
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-18 15:22:54 -0300

    Revert "Revert "feat: Req 04 -- OK -- fixed after Andre instructions""
    This reverts commit 112b1c4d0045e219e5d790ce38a01a777d1745b5.

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const tokenAuth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+    // console.log(token);
     if (!token || token === '') {
       return res.status(401).json({ message: 'Token not found' });
     }
