92321a4e81995b0fbcaade58ffd702a6f522e229
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-04-27 18:28:10 -0300

    chore: Adds the unfinished req 07

--- services/blogPost.js
+++ services/blogPost.js
@@ -9,9 +9,28 @@ const createBlogPost = async ({ title, content, categoryIds }) => {
   }
 };
 
+// const createBlogPost = async ({ id, title, content, categoryIds }) => {
+//   try {
+//     const categoryPromises = await Promise.all(categoryIds.map(async (categoryId) => { // categoryIds é um array --> promise
+//       const categories = await Category.findAll({ categoryId });
+//       return categories;
+//     })); 
+//     const categories = categoryPromises[0];
+//     const arrayOfIds = categories.map((category) => category.dataValues.id); // console.log(arrayOfIds); // PEGOU OS VALORES NUM ARRAY!
+//     if (arrayOfIds.some((categoryId) => categoryId == null)) {
+//       throw new Error('"categoryId" not found');
+//     }
+//     const blogPost = { title, content, id, categoryIds: arrayOfIds };
+//     const newBlogPost = await BlogPost.create(blogPost);
+//     return newBlogPost;    
+//   } catch (error) {
+//     console.error(error);    
+//   }
+// };
+
 const getAllBlogPosts = async () => {
   try {
-    const blogPosts = await BlogPost.findAll({ // eager loading
+    const blogPosts = await BlogPost.findAll({ // parece o do course
       include: [{
         model: User,
         as: 'user',
@@ -30,7 +49,7 @@ const getAllBlogPosts = async () => {
 
 const getBlogPostById = async (id) => {
   try {
-    const blogPost = await BlogPost.findByPk({ // findByPk não aceita o where junto
+    const blogPost = await BlogPost.findOne({ // findByPk não aceita o where junto
       include: [{
         model: User,
         as: 'user',
